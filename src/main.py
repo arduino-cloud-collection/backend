f0ec2c89fd9ae5e60eedbac7bb3271c5d7f3a99b
Yannis Storrer <yannis.storrer@web.de>
2020-10-05 16:31:52 +0200

    added run config for pycharm

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from src.routers import user, auth
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(user.router, prefix="/user")
 app.include_router(auth.router, prefix="/auth")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/settings.py
+++ src/settings.py
@@ -1,19 +1,12 @@
-from os import environ
+from os import getenv
 
 
 class EnvConfig:
     def __init__(self):
-        self.DATABASE_URL = self._get_env("DATABASE_URL")
-        self.JWT_KEY = self._get_env("JWT_KEY")
-        self.ALGORITHM = self._get_env("ALGORITHM")
-        self.ACCESS_TOKEN_EXPIRE_MINUTES = int(self._get_env("ACCESS_TOKEN_EXPIRE_MINUTES"))
-
-    def _get_env(self, env: str):
-        try:
-            env_var = environ[env]
-        except KeyError:
-            raise Exception("The enviroment variable " + env + " cannot be empty")
-        return env_var
+        self.DATABASE_URL = getenv("DATABASE_URL")
+        self.JWT_KEY = getenv("JWT_KEY")
+        self.ALGORITHM = getenv("ALGORITHM")
+        self.ACCESS_TOKEN_EXPIRE_MINUTES = int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 config = EnvConfig()
