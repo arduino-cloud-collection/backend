1aa4cae7e600f52ab515587c2bdc045524b6de70
Yannis Storrer <yannis.storrer@web.de>
2020-10-08 17:49:53 +0200

    Added setup dependencies

--- setup.py
+++ setup.py
@@ -10,4 +10,14 @@ setup(
     author='Yannis Storrer',
     author_email='yannis.storrer@web.de',
     description='The backend for the arduino-cloud project',
+    install_requires=[
+        'fastapi',
+        'uvicorn',
+        'sqlalchemy',
+        'psycopg2-binary',
+        'python-jose',
+        'python-multipart',
+        'blake3',
+        'bcrypt'
+    ]
 )
