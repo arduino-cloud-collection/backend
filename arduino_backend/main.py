a45a3c9be8c3926c97464c65acf59aff02c3241b
Yannis Storrer <yannis.storrer@web.de>
2020-10-20 23:45:59 +0200

    Added CORS support

--- arduino_backend/main.py
+++ arduino_backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from arduino_backend.routers import user, auth
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
 
 app.include_router(user.router, prefix="/user")
 app.include_router(auth.router, prefix="/auth")
