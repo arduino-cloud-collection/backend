c81311cc6330a2ba0e11f200bd9518aaa954595b
Yannis Storrer <yannis.storrer@web.de>
2020-10-25 09:33:37 +0100

    Renewed setuo

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 setup(
     name='arduino_backend',
     version='0.1',
-    packages=['arduino_backend', 'arduino_backend.test', 'arduino_backend.models', 'arduino_backend.routers',
-              'arduino_backend.schemas'],
+    packages=['arduino_backend', 'arduino_backend.user', 'arduino_backend.auth'],
     url='https://github.com/arduino-cloud-collection/backend',
     license='GPL',
     author='Yannis Storrer',
