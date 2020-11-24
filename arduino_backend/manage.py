f9e9a13d2a44c99922f03fc6637b8190f5282453
Yannis Storrer <yannis.storrer@web.de>
2020-11-24 23:07:46 +0100

    Added Database migrations

---
+++ arduino_backend/manage.py
@@ -0,0 +1,10 @@
+import typer
+
+from fastapi_migrations.cli import MigrationsCli
+
+app: typer.Typer = typer.Typer()
+
+app.add_typer(MigrationsCli())
+
+if __name__ == '__main__':
+    app()
--- arduino_backend/settings.py
+++ arduino_backend/settings.py
@@ -3,11 +3,11 @@ from os import getenv
 
 class EnvConfig:
     def __init__(self):
-        self.DATABASE_URL = getenv("DATABASE_URL")
-        self.JWT_KEY = getenv("JWT_KEY")
-        self.ALGORITHM = getenv("ALGORITHM")
-        self.ACCESS_TOKEN_EXPIRE_MINUTES = int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-        self.PEPPER = getenv("PEPPER")
+        self.DATABASE_URL = getenv("DATABASE_URL", "sqlite:///test.db")
+        self.JWT_KEY = getenv("JWT_KEY", "b7c18b06a0416d6af22bfaf49abaf7b75956056963e3116e3bbff3be72ed20fa")
+        self.ALGORITHM = getenv("ALGORITHM", "HS256")
+        self.ACCESS_TOKEN_EXPIRE_MINUTES = int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 10))
+        self.PEPPER = getenv("PEPPER", "hcQoBj+~Z#JxLd0K")
 
 
 config = EnvConfig()
