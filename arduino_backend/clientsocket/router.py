bd5f5ce614e4624d37d1ebec58f490d084d2f799
Yannis Storrer <yannis.storrer@web.de>
2020-12-24 11:04:40 +0100

    Added token Depends object.

--- arduino_backend/clientsocket/router.py
+++ arduino_backend/clientsocket/router.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, Depends, status
 
+from arduino_backend.token.models import Token
 
 router = APIRouter()
 
 
 @router.websocket_route("/")
-async def client_socket(websocket: WebSocket):
+async def client_socket(websocket: WebSocket, token: str = Depends(Token.get_token)):
     await websocket.accept()
     data = await websocket.receive_text()
     await websocket.send_text(f"Message text was: {data}")
